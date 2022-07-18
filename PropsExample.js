dddf45d40cc162d2c268c72ece403f9ab430c905
yuvrajsendhav <104247355+yuvrajsendhav@users.noreply.github.com>
2022-07-18 12:34:03 +0530

    Update PropsExample.js

--- PropsExample.js
+++ PropsExample.js
@@ -1,32 +1,10 @@
-import React, { useState } from "react";
-let PropsExample = () =>
-{
-    var [a,setA] = useState("white");
-    const bgcolor1 = () =>
-    {
-        setA("red")
-    }
-    const bgcolor2 = () =>
-    {
-        setA("blue")
-    }
-    const bgcolor3 = () =>
-    {
-        setA("green")
-    }
-    const bgcolor4 = () =>
-    {
-        setA("cyan")
-    }
-    return(
-         <div style={{ backgroundColor: a,height: '400px',}}
-        >
-        <p>Color</p>
-         <button className="btn" onClick={bgcolor1}>RED</button>
-         <button className="btn" onClick={bgcolor2}>BLUE</button>
-         <button  className="btn" onClick={bgcolor3}>GREEN</button>
-         <button className="btn"  onClick={bgcolor4}>CYAN</button>
-        </div>    
+let PropsExample = (props) =>
+{   
+    return(<div style={{backgroundColor:props.scs}}>
+       <h1>{props.scs}</h1>
+    
+         
+        </div>   
     )
 }
 export default PropsExample;
